595340fa7c4458083dd871e8f814b0134df9cab7
Johnson-Fan <fanzixiao@canaan-creative.com>
2016-08-23 16:06:46 +0800

    Add deal adc data function

--- User/Appl/main.c
+++ User/Appl/main.c
@@ -1,15 +1,77 @@
 #include "Includes.h"
 
+static u16 adc_buf[ADC_COUNT][SAMPLE_COUNT];
+static f32 vol_buf[ADC_COUNT];
+static u16 adc_avg[ADC_COUNT];
+
+u16 get_adc_avg(u16 *data, u8 len)
+{
+	u16 sum = 0;
+	u16 max = 0;
+	u16 min = 0;
+	u16 i = 0;
+
+	if (len < 3)
+		return 0;
+
+	max = *data;
+	min = *data;
+	for (i = 0; i < len; i++) {
+		max = max > *data ? max : *data;
+		min = min < *data ? min : *data;
+		sum += *data;
+		data++;
+	}
+
+	return ((sum - max - min) / (len -2));
+}
+
 int main(void)
 {
+	u8 i = 0;
+
 	gpio_init();
 	uart_init();
 	i2c_init();
 	adc_init();
 	timer_init();
 
+	set_led(LED_GREEN, LED_ON);
 	while (1)
 	{
-		;
+		if (get_adc_switch_state()) {
+			clear_adc_switch_state();
+			set_led(LED_YELLOW, LED_ON);
+			for (i = 0; i < SAMPLE_COUNT; i++) {
+				read_max34409_adc_data();
+				adc_buf[0][i] = max34409_data.adc1;
+				adc_buf[1][i] = max34409_data.adc2;
+				adc_buf[2][i] = max34409_data.adc3;
+				adc_buf[3][i] = max34409_data.adc4;
+				adc_buf[4][i] = SamplingData.adc1;
+				adc_buf[5][i] = SamplingData.adc2;
+			}
+
+			for (i = 0; i < 4; i++) {
+				adc_avg[i] = get_adc_avg(adc_buf[i], SAMPLE_COUNT);
+				vol_buf[i] = CURRENT_STD_VALUE /256.0 * (f32)adc_avg[i];
+			}
+
+			adc_avg[4] = get_adc_avg(adc_buf[4], SAMPLE_COUNT);
+			adc_avg[5] = get_adc_avg(adc_buf[5], SAMPLE_COUNT);
+
+			if (adc_avg[4] > adc_avg[5])
+				vol_buf[4] = (adc_avg[4] - adc_avg[5]) * 3.3 / 4096.0 * CURRENT_K;
+			else
+				vol_buf[4] = (adc_avg[5] - adc_avg[4]) * 3.3 / 4096.0 * CURRENT_K;
+
+			printf("VDDIOA: %.00f\r\n", vol_buf[0]);
+			printf("VDDIOB: %.00f\r\n", vol_buf[1]);
+			printf("VDDPLL: %.00f\r\n", vol_buf[2]);
+			printf("VDDTOP: %.00f\r\n", vol_buf[3]);
+			printf("VIOUT:  %.00f\r\n", vol_buf[4]);
+
+			set_led(LED_YELLOW, LED_OFF);
+		}
 	}
 }
--- User/Header/Config.h
+++ User/Header/Config.h
@@ -3,5 +3,10 @@
 
 typedef float  f32;
 
+#define ADC_COUNT         6
+#define SAMPLE_COUNT      10
+#define CURRENT_STD_VALUE 100.0f /* 10mv ÷0.1Ω*/
+#define CURRENT_K         10
+
 #endif
 
